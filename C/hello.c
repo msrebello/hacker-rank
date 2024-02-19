1804fa229d0c3d31f9ab6cb4083c047d74f1ade1
matheus rebello <matheussrb@outlook.com>
2024-02-19 10:30:54 -0300

    add solution: "Hello World!" in C

---
+++ C/hello.c
@@ -0,0 +1,17 @@
+// https://www.hackerrank.com/challenges/hello-world-c/problem?isFullScreen=true
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() 
+{
+	
+    char s[100];
+    scanf("%[^\n]%*c", &s);
+    printf("Hello, World!\nWelcome to C programming.");
+  	
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
+    return 0;
+}
