1827108f1fbdc556dca0389c581bbd5e7196c812
matheus rebello <matheussrb@outlook.com>
2024-02-23 09:46:22 -0300

    renamed class and added corresponding folder

---
+++ Java/src/welcome/Solution.java
@@ -0,0 +1,11 @@
+// https://www.hackerrank.com/challenges/welcome-to-java/problem?isFullScreen=true
+
+package welcome;
+
+public class Solution {
+    public static void main(String[] args) {
+
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+    }
+}
