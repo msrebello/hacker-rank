7f80c0f3080055b324e06011c80bdbfd5e7a6cdb
matheus rebello <matheussrb@outlook.com>
2024-02-23 22:00:37 -0300

    add solution: Bitwise Operators

---
+++ C/bitwise.c
@@ -0,0 +1,40 @@
+// https://www.hackerrank.com/challenges/bitwise-operators-in-c/problem?isFullScreen=true
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+void calculate_the_maximum(int n, int k)
+{
+
+    int max_and = 0, max_or = 0, max_xor = 0;
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = i + 1; j <= n; j++)
+        {
+            int and = i & j;
+            int or = i | j;
+            int xor = i ^ j;
+
+            if (and > max_and && and < k)
+                max_and = and;
+            if (or > max_or && or < k)
+                max_or = or ;
+            if (xor > max_xor && xor < k)
+                max_xor = xor;
+        }
+    }
+
+    printf("%d\n%d\n%d\n", max_and, max_or, max_xor);
+}
+
+int main()
+{
+    int n, k;
+
+    scanf("%d %d", &n, &k);
+    calculate_the_maximum(n, k);
+
+    return 0;
+}
